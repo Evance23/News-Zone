27b7eb74d8be32078b5e7d99cfa280bb6449bde4
Evance23 <evancebarracks01@gmail.com>
2021-04-19 05:43:43 +0300

    Add all modified files

---
+++ app/models/news.py
@@ -0,0 +1,11 @@
+
+
+class News:
+    '''
+    news class to define News Objects
+    '''
+
+    def __init__(self,id,title,overview):
+        self.id =id
+        self.title = title
+        self.overview = overview
