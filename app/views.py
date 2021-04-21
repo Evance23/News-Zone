8605467a795aab9b7046a2c092ffcf2a36bc09c1
Evance23 <evancebarracks01@gmail.com>
2021-04-21 04:21:55 +0300

    Add search query function

--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
 from flask import render_template
 from app import app
 # from newsapi import NewsApiClient
-from .request import get_news,
+from .request import get_news
+from .requests import get_news, get_news, search_news
+from flask import render_template, request, redirect.url_for
 
 
 # Views
@@ -22,17 +24,34 @@ def index():
 
     # Getting popular news
     popular_news = get_news('popular')
-    print(popular_news)
+    # print(popular_news)
     title = "Home- Welcome to the Home of News Updates"
+    search_news = request.args.get('news_query')
+
+    if search_news:
+        return redirect(url_for('search', news_name=search_news))
+    else:
+
     return render_template('index.html', title=title, technology=technology, science=science, politics=politics, sports=sports, business=business. entertainment=entertainment')
 
 
 @app route('/news/<int:id>')
 def movie(id):
 
-
     news = get_news(id)
     title = f'{news.title}'
 
     return render_template('news.html', title=title, news=news)
 
+
+@app.route('/search/<news_name>')
+def search(news_name):
+    '''
+    View function to display the search results
+    '''
+    news_name_list = news_name.split(" ")
+    news_name_format = "+".join(news_name_list)
+    searched_news = search_news(news_name_format)
+    title = f'search results for {news_name}'
+
+    return render_template('search.html', news=searched_news)
